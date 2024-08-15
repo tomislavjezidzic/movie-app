d2a38e3a8634d61f692176970cdbddd86c42cd87
tomo <tjezidzic@outlook.com>
2024-08-15 18:34:05 +0200

    redirects

--- next.config.js
+++ next.config.js
@@ -10,6 +10,15 @@ const modules = {
     images: {
         domains: ['picsum.photos', 'image.tmdb.org'],
     },
+    async redirects() {
+        return [
+            {
+                source: '/movie',
+                destination: '/most-watched',
+                permanent: true,
+            },
+        ];
+    },
 };
 
 module.exports = modules;
