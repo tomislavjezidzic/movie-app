b2827ac88ff3007caa9163841bf664223bae5b4d
tomo <tjezidzic@outlook.com>
2024-08-20 19:21:15 +0200

    error handling

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,80 +33,88 @@ const IndexPage = ({ newest, top, popularAnimation, popularAction }: HomepagePro
 
 export const getStaticProps: GetStaticProps = async () => {
     const newestResponse = await getNewest();
+    const newest = (newestResponse as AxiosResponse<any, any>)?.data?.results;
 
     const popularActionResponse = await getMostWatched(28, null, null, 1);
+    const popularAction = (popularActionResponse as AxiosResponse<any, any>)?.data?.results;
 
     const popularAnimationResponse = await getMostWatched(16, null, null, 1);
+    const popularAnimation = (popularAnimationResponse as AxiosResponse<any, any>)?.data?.results;
 
     const topResponse = await getTopRated();
+    const top = (topResponse as AxiosResponse<any, any>)?.data?.results;
 
     return {
         props: {
             title: 'Movie App Homepage',
-            newest: (newestResponse as AxiosResponse<any, any>)?.data?.results
-                ?.slice(0, 8)
-                .map((item: MovieCardPropsResponse): MovieCardProps => {
-                    return {
-                        image: {
-                            src: `${process.env.TMDB_IMAGES_BASE_URL_ENDPOINT}/w400${item.poster_path}`,
-                            alt: item.title,
-                        },
-                        title: item.title,
-                        score: parseFloat(item.vote_average).toFixed(2),
-                        slug: `${item.id}-${slugify(item.title, {
-                            strict: true,
-                        }).toLowerCase()}`,
-                        id: item.id,
-                    };
-                }),
-            popularAction: (popularActionResponse as AxiosResponse<any, any>)?.data?.results
-                ?.slice(0, 10)
-                .map((item: MovieCardPropsResponse): MovieCardProps => {
-                    return {
-                        image: {
-                            src: `${process.env.TMDB_IMAGES_BASE_URL_ENDPOINT}/w200${item.poster_path}`,
-                            alt: item.title,
-                        },
-                        title: item.title,
-                        score: parseFloat(item.vote_average).toFixed(2),
-                        slug: `${item.id}-${slugify(item.title, {
-                            strict: true,
-                        }).toLowerCase()}`,
-                        id: item.id,
-                    };
-                }),
-            popularAnimation: (popularAnimationResponse as AxiosResponse<any, any>)?.data?.results
-                ?.slice(0, 10)
-                .map((item: MovieCardPropsResponse): MovieCardProps => {
-                    return {
-                        image: {
-                            src: `${process.env.TMDB_IMAGES_BASE_URL_ENDPOINT}/w200${item.poster_path}`,
-                            alt: item.title,
-                        },
-                        title: item.title,
-                        score: parseFloat(item.vote_average).toFixed(2),
-                        slug: `${item.id}-${slugify(item.title, {
-                            strict: true,
-                        }).toLowerCase()}`,
-                        id: item.id,
-                    };
-                }),
-            top: (topResponse as AxiosResponse<any, any>)?.data?.results
-                ?.slice(0, 3)
-                .map((item: MovieCardPropsResponse): MovieCardProps => {
-                    return {
-                        image: {
-                            src: `${process.env.TMDB_IMAGES_BASE_URL_ENDPOINT}/w400${item.poster_path}`,
-                            alt: item.title,
-                        },
-                        title: item.title,
-                        score: parseFloat(item.vote_average).toFixed(2),
-                        slug: `${item.id}-${slugify(item.title, {
-                            strict: true,
-                        }).toLowerCase()}`,
-                        id: item.id,
-                    };
-                }),
+            newest: !newest
+                ? []
+                : newest?.slice(0, 8).map((item: MovieCardPropsResponse): MovieCardProps => {
+                      return {
+                          image: {
+                              src: `${process.env.TMDB_IMAGES_BASE_URL_ENDPOINT}/w400${item.poster_path}`,
+                              alt: item.title,
+                          },
+                          title: item.title,
+                          score: parseFloat(item.vote_average).toFixed(2),
+                          slug: `${item.id}-${slugify(item.title, {
+                              strict: true,
+                          }).toLowerCase()}`,
+                          id: item.id,
+                      };
+                  }),
+            popularAction: !popularAction
+                ? []
+                : popularAction
+                      ?.slice(0, 10)
+                      .map((item: MovieCardPropsResponse): MovieCardProps => {
+                          return {
+                              image: {
+                                  src: `${process.env.TMDB_IMAGES_BASE_URL_ENDPOINT}/w200${item.poster_path}`,
+                                  alt: item.title,
+                              },
+                              title: item.title,
+                              score: parseFloat(item.vote_average).toFixed(2),
+                              slug: `${item.id}-${slugify(item.title, {
+                                  strict: true,
+                              }).toLowerCase()}`,
+                              id: item.id,
+                          };
+                      }),
+            popularAnimation: !popularAnimation
+                ? []
+                : popularAnimation
+                      ?.slice(0, 10)
+                      .map((item: MovieCardPropsResponse): MovieCardProps => {
+                          return {
+                              image: {
+                                  src: `${process.env.TMDB_IMAGES_BASE_URL_ENDPOINT}/w200${item.poster_path}`,
+                                  alt: item.title,
+                              },
+                              title: item.title,
+                              score: parseFloat(item.vote_average).toFixed(2),
+                              slug: `${item.id}-${slugify(item.title, {
+                                  strict: true,
+                              }).toLowerCase()}`,
+                              id: item.id,
+                          };
+                      }),
+            top: !top
+                ? []
+                : top?.slice(0, 3).map((item: MovieCardPropsResponse): MovieCardProps => {
+                      return {
+                          image: {
+                              src: `${process.env.TMDB_IMAGES_BASE_URL_ENDPOINT}/w400${item.poster_path}`,
+                              alt: item.title,
+                          },
+                          title: item.title,
+                          score: parseFloat(item.vote_average).toFixed(2),
+                          slug: `${item.id}-${slugify(item.title, {
+                              strict: true,
+                          }).toLowerCase()}`,
+                          id: item.id,
+                      };
+                  }),
         },
         revalidate: 3600,
     };
