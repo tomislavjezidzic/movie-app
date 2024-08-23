d05823373bdae7937cf9c9d49f4d713c2193aa03
tomo <tjezidzic@outlook.com>
2024-08-23 09:35:44 +0200

    marquee fix

--- components/organisms/Marquee/Marquee.tsx
+++ components/organisms/Marquee/Marquee.tsx
@@ -5,16 +5,24 @@ export interface MarqueeProps {
     content: string[];
 }
 
-const Marquee = ({ content }: MarqueeProps) => (
-    <section className={cn(styles.wrapper, 'o-section', 'u-a4')}>
-        {[0, 1, 2, 3, 4].map(index => (
-            <div className={styles.mover} key={`marquee-mover-${index}`}>
-                {content?.map((item, key) => (
-                    <span key={`marquee-item-${index}-${key}`}>{item}</span>
-                ))}
-            </div>
-        ))}
-    </section>
-);
+const Marquee = ({ content }: MarqueeProps) => {
+    let keysArray = [0, 1, 2, 3, 4];
+
+    if (content.length < 3) {
+        keysArray = [0, 1, 2, 3, 4, 5, 6];
+    }
+
+    return (
+        <section className={cn(styles.wrapper, 'o-section', 'u-a4')}>
+            {keysArray.map(index => (
+                <div className={styles.mover} key={`marquee-mover-${index}`}>
+                    {content?.map((item, key) => (
+                        <span key={`marquee-item-${index}-${key}`}>{item}</span>
+                    ))}
+                </div>
+            ))}
+        </section>
+    );
+};
 
 export default Marquee;
