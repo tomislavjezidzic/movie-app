36424d9c0637b419f760da5beef192a1e7f7f0ce
tomo <tjezidzic@outlook.com>
2024-08-24 14:25:07 +0200

    response caching

--- libs/movieClient.ts
+++ libs/movieClient.ts
@@ -1,13 +1,17 @@
 import axios from 'axios';
 import * as console from 'node:console';
+import { setupCache } from 'axios-cache-interceptor';
+
 
 const axiosClient = axios.create({
     baseURL: process.env.TMDB_BASE_URL_ENDPOINT,
 });
 
+const Axios = setupCache(axiosClient);
+
 export async function getMostWatched(genreId: number, year: number, score: number, page?: number) {
     const dynamicQuery = `${year && `&year=${year}`}${score && `&vote_average.gte=${score}`}&page=${page ? page : 1}${genreId && `&with_genres=${genreId}`}`;
-    return await axiosClient
+    return await Axios
         .get(
             `/discover/movie?include_adult=false&include_video=false&language=en-US${dynamicQuery}&sort_by=popularity.desc&api_key=${process.env.TMDB_API_KEY}`
         )
@@ -17,7 +21,7 @@ export async function getMostWatched(genreId: number, year: number, score: numbe
 }
 
 export async function getNewest() {
-    return await axiosClient
+    return await Axios
         .get(`/movie/now_playing?language=en-US&page=1&api_key=${process.env.TMDB_API_KEY}`)
         .catch(error => {
             console.log(`Newest endpoint error: ${error}`);
@@ -25,7 +29,7 @@ export async function getNewest() {
 }
 
 export async function getTopRated() {
-    return await axiosClient
+    return await Axios
         .get(`/movie/top_rated?language=en-US&page=1&api_key=${process.env.TMDB_API_KEY}`)
         .catch(error => {
             console.log(`Top rated endpoint error: ${error}`);
@@ -33,7 +37,7 @@ export async function getTopRated() {
 }
 
 export async function getMovie(movieId: number) {
-    return await axiosClient
+    return await Axios
         .get(
             `/movie/${movieId}?append_to_response=credits&language=en-US&with_cast=true&api_key=${process.env.TMDB_API_KEY}`
         )
@@ -43,7 +47,7 @@ export async function getMovie(movieId: number) {
 }
 
 export async function getQueriedMovies(searchQuery: string, page?: string) {
-    return await axiosClient
+    return await Axios
         .get(
             `/search/movie?query=${searchQuery}&page=${page || '1'}&include_adult=false&language=en-US&api_key=${process.env.TMDB_API_KEY}`
         )
